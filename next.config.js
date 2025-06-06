bc84d54be866cac355d73dc945bec71baaa00479
Alexander <rogericeee@gmail.com>
2025-06-06 15:28:03 +0300

    Add next.config.js 1749212883673

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/wikipedia/commons/**',
+      },
+    ],
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
